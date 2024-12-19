a366a286f824a28278efb2c8aa5c61d9e50c468c
SaidakbarX <khayumkhujayev6937@gmail.com>
2024-12-19 18:59:20 +0500

    update

--- Homework2.8/Homework2.8/Repositories/ISchoolRepository.cs
+++ Homework2.8/Homework2.8/Repositories/ISchoolRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace Homework2._8.Repositories
+﻿using Homework2._8.DataAccess.Entities;
+
+namespace Homework2._8.Repositories
 {
     public interface ISchoolRepository
     {
+        void DeleteSchool(Guid Id);
+        School ReadSchoolById(Guid Id);
+        List<School> ReadSchools();
+        void UpdateSchool(School school);
+        Guid WriteSchool(School school);
+
     }
 }
